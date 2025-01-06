c4221919e37afca0bc28863415cba8f125cded8a
orionexus <sebastianvargaslopez36@gmail.com>
2025-01-06 00:25:04 -0500

    implement Spotify response to get playlist tracks

--- app/routes/mood_routes.py
+++ app/routes/mood_routes.py
@@ -1,29 +1,32 @@
 from fastapi import APIRouter, Request, HTTPException,status
-from ..models.models import MoodRequest
+from app.models.models import MoodRequest
 from app.services.spotify_service import get_recommended_tracks
 from app.services.mood_analysis import analyze_sentiment
 
 router = APIRouter()
 
-# Here we will get the text from the user and return the mood using analyze_sentiment()
+# Get the text from the user and return the mood using analyze_sentiment()
 @router.post("/analyze-mood")
 async def analyze_mood(text: MoodRequest):
     
-    mood = analyze_sentiment(text.text) # Here we will use NLP to analyze the user mood
+    mood = analyze_sentiment(text.text) # The NLP analyzes the user's mood
+    
     return {
     "data": {"text": text.text, "mood": mood},
     "message": "Welcome to my Spotify App <a href='/mood/authorize'>Login with Spotify</a>"
-}
+    }
 
 
 @router.get("/recommend-playlist")
 async def recommend_playlist(request: Request, mood: str):
+    
+    # Cannot access the endpoint if not authenticated with Spotify
     if 'access_token' not in request.session:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= 'You are not logged in to Spotify')
-    # This endpoint will generate a playlist based on the user's mood
+        
+    access_token = request.session['access_token']
     
-    # Here we will show the recommendations (We will work on this later on)
-    playlist = get_recommended_tracks(mood) 
-    return {"mood": mood, "playlist": playlist}
+    recommendations = get_recommended_tracks(mood, access_token) # Get 3 songs based on mood
+    
+    return {"mood": mood, "playlist": recommendations}
 
-    
--- app/services/spotify_service.py
+++ app/services/spotify_service.py
@@ -1,43 +1,51 @@
 import requests
+import random
 
 # Playlists IDs
 SAD = '0qaIzNNRaEZzkG0bfYlYg3'
 NEUTRAL = '48gsC1F8hqKTfsuvLnvZbW'
 HAPPY = '3Hs562oYeOsTcNmiUSj0f8'
 
-# Asigning a playlist to each possible mood
+# Assign a playlist ID to each possible mood
 MOOD_PLAYLISTS  = {
-    "sad":       {f'https://api.spotify.com/v1/playlists/{SAD}'},
-    "very sad":  {f'https://api.spotify.com/v1/playlists/{SAD}'},
-    "neutral":   {f'https://api.spotify.com/v1/playlists/{NEUTRAL}'},
-    "happy":     {f'https://api.spotify.com/v1/playlists/{HAPPY}'},
-    "very happy":{f'https://api.spotify.com/v1/paylists/{HAPPY}'},
+    "sad":       SAD,
+    "very sad":  SAD,
+    "neutral":   NEUTRAL,
+    "happy":     HAPPY,
+    "very happy": HAPPY,
 }
 
 
+# Filter Spotify response to get 3 random songs
+def filter_spotify_songs(playlist_response:dict):
+    tracks = random.sample(playlist_response['tracks']['items'], k= 3)
+    result = []
+    
+    for item in tracks:
+        track_info = {
+            'track_name' : item['track']['name'],
+            'artist_name' : [artist['name'] for artist in item['track']['artists']],
+            'spotify_url' : item['track']['external_urls']['spotify']
+        }
+        
+        result.append(track_info)
+        
+    return result
+
+    
 def get_recommended_tracks(mood: str, access_token: str):
+    headers = {'Authorization': f'Bearer {access_token}'}
     
-    headers = {'authorization' f'Bearer {access_token}'}
-    response = requests.get(f'{MOOD_PLAYLISTS[mood]}', headers= headers)
+    response = requests.get(f'https://api.spotify.com/v1/playlists/{MOOD_PLAYLISTS[mood]}', headers= headers)
+    
     result = response.json()
-    items = response['tracks']['items']
-
-    headers = {"Authorization": f"Bearer {access_token}"}
-    response = requests.get("sad", headers=headers)
-    response.raise_for_status()
-    return response.json()
-
-    """
-    Get a list of recommended tracks based on the user's mood.
-    """
-    mood_to_genre = {
-        "happy": "pop",
-        "very happy": "dance",
-        "neutral": "acoustic",
-        "sad": "piano",
-        "very sad": "ambient"
-    }
     
-    # implement logic with Spotify API to return the accurate tracks based on mood...
+    filtered = filter_spotify_songs(result)
+    
+    return filtered
+
+
+
+
 
     
